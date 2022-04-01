55d3f6c8b544a617a963ac8b13fa0179ab8fabfd
Juan Molano <je.molano1498@gmail.com>
2022-04-01 11:25:47 +0200

    Single steps approach

--- data_analysis/RunningEnv.py
+++ data_analysis/RunningEnv.py
@@ -1,5 +1,4 @@
 from PaceSimulator import PaceSimulator
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -19,6 +18,9 @@ class Timer:
             return 0
         else:
             return 1
+
+    def get_remaining_percentage (self):
+        return 1-((self.time_limit - self.time_step)/self.time_limit)
 
 
 
@@ -49,6 +51,7 @@ class RunningEnv:
         self.current_timer = Timer(self.time_limit)
 
     def step(self, action, target_pace):
+        done = 0
         if action == 1:
             self.current_timer = Timer(self.time_limit)
             self.current_timer.tick()
@@ -62,74 +65,76 @@ class RunningEnv:
                 self.current_timer.tick()
 
         current_pace = self.simulator.predict(pacing_value, self.pref_pace, target_pace)
-        new_state = self.state.get_next_state(current_pace)
+        avg_pace = self.state.get_next_state(current_pace)
+        new_state = [(avg_pace/self.pref_pace) - 1, (2 * self.current_timer.get_remaining_percentage()) - 1]
 
-        reward = self.get_distance_reward(target_pace, current_pace) + self.get_pacing_reward()
+        reward = self.get_distance_reward(target_pace, avg_pace) - pacing_value
 
-        return current_pace, new_state, reward, pacing_value
+        return np.array([current_pace]), np.array(new_state), np.array([reward], dtype=np.float), np.array([pacing_value]), np.array([done])
+        # return np.array([current_pace]), np.array(new_state), np.array([reward], dtype=np.float), np.array([pacing_value]), np.array([done])
 
     def get_distance_reward(self, target_pace, current_pace):
-        return -abs(target_pace - current_pace) + 1
+        reward = 0
+        if abs(target_pace - current_pace) > 4:
+            reward = -5
+        if abs(target_pace - current_pace) < 3:
+            reward = 1
+        if abs(target_pace - current_pace) < 2:
+            reward = 2
+        return reward
 
-    def get_pacing_reward(self):
-        if self.current_timer.timer_on():
-            return -1
-        else:
-            return 0
+    def reset (self):
+        self.state = EwmaBiasState()
+        self.simulator = PaceSimulator()
+        self.pace_active = 0
+        self.current_timer = Timer(self.time_limit)
 
-env = RunningEnv(181)
-state = np.empty(0)
-pace = np.empty(0)
-rewards = np.empty(0)
-env_pacing = np.empty(0)
-pacing = []
 
-for y in range(100):
-    value = 0
-    current_pace, new_state, reward, real_pacing = env.step(value, 181)
-    state = np.append(state, new_state)
-    pace = np.append(pace, current_pace)
-    rewards = np.append(rewards, reward)
-    env_pacing = np.append(env_pacing, real_pacing)
-    pacing.append(value)
-
-for y in range(50):
-    value = 1
-    current_pace, new_state, reward, real_pacing = env.step(value, 181)
-    state = np.append(state, new_state)
-    pace = np.append(pace, current_pace)
-    rewards = np.append(rewards, reward)
-    env_pacing = np.append(env_pacing, real_pacing)
-    pacing.append(value)
-
-for y in range(100):
-    value = 0
-    current_pace, new_state, reward, real_pacing = env.step(value, 181)
-    state = np.append(state, new_state)
-    pace = np.append(pace, current_pace)
-    rewards = np.append(rewards, reward)
-    env_pacing = np.append(env_pacing, real_pacing)
-    pacing.append(value)
-
-x = np.linspace(0, len(pacing), len(pacing))
-plt.scatter(x[np.array(env_pacing)==1], np.array(pace)[np.array(env_pacing)==1], marker="x", label='Paced steps')
-plt.scatter(x[np.array(env_pacing)==0], np.array(pace)[np.array(env_pacing)==0], marker="x", label='Not-paced steps')
-
-plt.scatter(x[np.array(pacing)==1], np.array(pacing)[np.array(pacing)==1]*181, color='r', marker="x")
-plt.axhline(y=181, color='k', linestyle='--', label='Target Pace')
-
-plt.plot(x, state, 'r-', linewidth=2)
-plt.show()
-
-print(rewards.mean())
+# env = RunningEnv(181)
+# state = np.empty(0)
+# pace = np.empty(0)
+# rewards = np.empty(0)
+# env_pacing = np.empty(0)
+# pacing = []
+#
+# for y in range(100):
+#     value = 0
+#     current_pace, new_state, reward, real_pacing = env.step(value, 181)
+#     state = np.append(state, new_state)
+#     pace = np.append(pace, current_pace)
+#     rewards = np.append(rewards, reward)
+#     env_pacing = np.append(env_pacing, real_pacing)
+#     pacing.append(value)
+#
+# for y in range(50):
+#     value = 1
+#     current_pace, new_state, reward, real_pacing = env.step(value, 181)
+#     state = np.append(state, new_state)
+#     pace = np.append(pace, current_pace)
+#     rewards = np.append(rewards, reward)
+#     env_pacing = np.append(env_pacing, real_pacing)
+#     pacing.append(value)
+#
+# for y in range(100):
+#     value = 0
+#     current_pace, new_state, reward, real_pacing = env.step(value, 181)
+#     state = np.append(state, new_state)
+#     pace = np.append(pace, current_pace)
+#     rewards = np.append(rewards, reward)
+#     env_pacing = np.append(env_pacing, real_pacing)
+#     pacing.append(value)
+#
+# x = np.linspace(0, len(pacing), len(pacing))
+# plt.scatter(x[np.array(env_pacing)==1], np.array(pace)[np.array(env_pacing)==1], marker="x", label='Paced steps')
+# plt.scatter(x[np.array(env_pacing)==0], np.array(pace)[np.array(env_pacing)==0], marker="x", label='Not-paced steps')
+#
+# plt.scatter(x[np.array(pacing)==1], np.array(pacing)[np.array(pacing)==1]*181, color='r', marker="x")
+# plt.axhline(y=181, color='k', linestyle='--', label='Target Pace')
+#
+# plt.plot(x, state, 'r-', linewidth=2)
+# plt.annotate("Hola", xy=(0.5,0.5), xycoords='figure points')
+# plt.show()
+#
+# print(rewards.mean())
 
 ##
-
-plt.axis([0, 10, 0, 1])
-
-for i in range(10):
-    y = np.random.random()
-    plt.scatter(i, y)
-    plt.pause(0.05)
-
-plt.show()
---
+++ data_analysis/render_graph.py
@@ -0,0 +1,59 @@
+import math
+
+import matplotlib; matplotlib.use("TkAgg")
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from RunningEnv import RunningEnv
+
+x=0
+FRAMES = 500
+pref_pace = 181
+env = RunningEnv(pref_pace, 1)
+pacing = 0
+total_reward = 0
+state = np.empty(0)
+x_acc = np.empty(0)
+
+current_plt = plt.figure()
+def onclick(event):
+    global pacing
+    pacing = not(pacing)
+    print('Pace %s' % ('Activated' if pacing else 'Dis-activated'))
+    # print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+    #       ('double' if event.dblclick else 'single', event.button,
+    #        event.x, event.y, event.xdata, event.ydata))
+
+cid = current_plt.canvas.mpl_connect('button_press_event', onclick)
+
+def animate(iFrame):
+    global x, x_acc, state, total_reward
+    x=x+1
+    current_pace, new_state, reward, real_pacing, done = env.step(pacing, pref_pace)
+    total_reward = total_reward + reward
+    x_acc = np.append(x_acc, x)
+    state = np.append(state, (new_state[0]+1)*pref_pace)
+    pace_color = 'b'
+    if real_pacing: pace_color = 'g'
+    plt.scatter(x, current_pace, marker="x", color=pace_color)
+    plt.plot(x_acc, state, 'r-', linewidth=1)
+    plt.annotate("Reward: %s"%(np.round(reward[0], 1)), xy=(1, 1), xycoords='figure points',
+                 bbox=dict(fc='white', ec='white'))
+    plt.annotate("Avg. Pace: %s"%(np.round((new_state[0]+1)*pref_pace, 0)), xy=(100, 1), xycoords='figure points',
+                 bbox=dict(fc='white', ec='white'))
+    plt.annotate("Total Reward: %s"%(np.round(total_reward[0], 2)), xy=(200, 1), xycoords='figure points',
+                 bbox=dict(fc='white', ec='white'))
+    plt.annotate("Remaining time: %s%%"%(np.round(new_state[1], 2)), xy=(300, 1), xycoords='figure points',
+                 bbox=dict(fc='white', ec='white'))
+
+ani = FuncAnimation(current_plt, animate, interval=10, frames=FRAMES, repeat=False)
+plt.axhline(y=pref_pace, color='k', linestyle='--', label='Target Pace')
+plt.title("Pace simulator")
+plt.xlabel("time")
+plt.ylabel("spm")
+
+plt.show()
+
+# ani.save(r'animation.gif', fps=10)
+
+
